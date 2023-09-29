9f88e97da53ec3b1c95bf86bb118d96de6912a7b
AmitSingh-techz5 <amitsinghtechz5@gmail.com>
2023-09-29 15:03:07 +0530

    Initial commit

--- src/main/java/com/Demo/demo/DemoApplication.java
+++ src/main/java/com/Demo/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
    //NewSpringboot proj
+	//2nd commit
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
